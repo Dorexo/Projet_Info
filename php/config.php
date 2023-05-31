cdd0957740aeb94e0a75720f3f5a071bf8988381
Emir YAVUZ <emir.yavuz@isen-ouest.yncrea.fr>
2023-05-31 16:08:28 +0200

    aaaaaaaa

--- php/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    const DB_USER = 'postgres';
-    const DB_PWD = 'Isen14';    # Change pswd
-    const DB_NAME = 'projetinfo';
-    const DB_SERVER = '127.0.0.1';
-    const DB_PORT = '5432';
-?>
