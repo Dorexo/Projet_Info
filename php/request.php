67efd8c9f826995427c0e26e46948c8b3b710618
Emir YAVUZ <emir.yavuz@isen-ouest.yncrea.fr>
2023-06-01 10:57:06 +0200

    a

--- php/request.php
+++ php/request.php
@@ -5,12 +5,14 @@
     require_once('database.php');
     $db = database::connexionBD();
   
+    $requestmethod = $_SERVER['REQUEST_METHOD'];
     $requestid = substr($_SERVER['PATH_INFO'], 1);
     $requestid = explode('/', $requestid);
     $requesttype = array_shift($requestid);
 
-    if($requesttype=="inscription"){
-        if($_SERVER['REQUEST_METHOD']=="POST"){
+
+    if($requesttype == "inscription"){
+        if($requestmethod == "POST"){
             $request = dbInsertNewUser($db,$_POST['nom'],$_POST['prenom'],$_POST['date_naissance'],$_POST['email'],$_POST['mdp']);
         }
     }
